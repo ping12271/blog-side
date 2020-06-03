22f07a3436bdeb7ced96231a2e6b01184756a9b4
shin hyeeun <ping12271@hanmail.net>
2020-06-03 15:35:08 +0900

    backtop,scroll-heder추가

--- js/script.js
+++ js/script.js
@@ -8,4 +8,53 @@ $(function () {
     $('.tags h3').on('click', function () {
         $('.tags a').slideToggle(200);
     })
+    /**
+     * Blog UI Script Initialize.
+     */
+    blog.init();
 })
+
+const blog = {
+    init: function () {
+        blog.toggleSlideMenu()
+        blog.getScroll()
+        blog.backTop()
+        blog.visualSlide();
+    },
+    toggleSlideMenu: function () {
+        $('.sidebar-item').on('click', function () {
+            $(this).find('ul, .tags-list').slideToggle();
+        })
+    },
+
+    getScroll: function () {
+      $(window).on('scroll', function () {
+            const scrollTop = $(Window).scrollTop();
+            const visualHeight = $('.visual').innerHeight();
+            if(scrollTop > 100) {
+                $('html').addClass('get-scroll')
+            } else {
+                $('html').removeClass('get-scroll')
+            }
+          if(scrollTop > visualHeight) {
+              $('html').addClass('show-header')
+          } else {
+              $('html').removeClass('show-header')
+          }
+      })
+    },
+
+
+
+
+    backTop: function () {
+        $('.back-top').on('click', function () {
+            $('html, body').animate({
+                scrollTop: 0
+            })
+        })
+    }
+
+
+
+}
